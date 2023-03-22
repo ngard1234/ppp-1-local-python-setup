7258c740bd13bfcd7fb9cc42b8d600ca7413a1bb
ngard1234 <neilgardner@verizon.net>
2023-03-22 16:55:50 -0400

    correct eat_lunch code.

--- ppp-1-local-python-setup/functions_practice.py
+++ ppp-1-local-python-setup/functions_practice.py
@@ -4,28 +4,58 @@ def hello():
 def pack(a, b, c):
    return [ a, b, c ]
 
+#read instruction incorrect. Assume when everthing is eaten from the lunchbox a declaration of " My lunch box is empty"
 def eat_lunch(any_list):
-
     print(f"First I eat {any_list[0]}")
-
     for i in range (1, len(any_list)+1):
-      
         if i == len(any_list):
             print(f"My lunchbox is empty")
         else:
             print(f"Next I eat {any_list[i]}")
 
 
+def my_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+    
+
+def l_lunch(lunch):
+   if len(lunch)==0:
+        print("My lunchbox is empty!")
+   else:
+    for i in range (len(lunch)):
+        if i>= 0:
+            print(f"First I eat {lunch[i]}") 
+        else:  
+            print(f"Next I eat {lunch[i]}")
+      
+
+
+
+
 
 hello()
 
-print(pack('amy','bob','sue'))
+# print(pack('amy','bob','sue'))
 
-lunch_list1 = ['salad', 'soup', 'pizza', 'pudding']
+# lunch_list1 = ['salad', 'soup', 'pizza', 'pudding']
 lunch_list2 = [ 'sushi', 'eggroll', 'tofu', 'peking duck', 'fried rice']
 
-eat_lunch(lunch_list1)
+# eat_lunch(lunch_list1)
 
-eat_lunch(lunch_list2)
+# my_lunch(lunch_list2)
 
-eat_lunch(['bread', 'croissant', 'baguette', 'brioche'])
+
+
+l_lunch(['bread', 'croissant', 'baguette', 'brioche'])
+
+
+
+
